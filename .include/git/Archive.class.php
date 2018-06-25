01f61510129f4fccf8e13973441d26d80f1cce24
Aleksandr Izmailov <ad.izmailov@gmail.com>
2018-06-25 18:18:24 +0300

    removed undefined constant usage

--- .include/git/Archive.class.php
+++ .include/git/Archive.class.php
@@ -22,429 +22,405 @@ class GitPHP_Archive
     const GITPHP_COMPRESS_BZ2 = 'tbz2';
     const GITPHP_COMPRESS_GZ = 'tgz';
     const GITPHP_COMPRESS_ZIP = 'zip';
+
     /**
-	 * gitObject
-	 *
-	 * Stores the object for this archive internally
-	 *
-	 * @access protected
-	 */
-	protected $gitObject = null;
+     * Stores the object for this archive internally
+     * @var \GitPHP_GitObject
+     */
+    protected $gitObject = null;
 
-	/**
-	 * project
-	 *
-	 * Stores the project for this archive internally
-	 *
-	 * @access protected
-	 */
-	protected $project = null;
+    /**
+     * Stores the project for this archive internally
+     */
+    protected $project = null;
 
-	/**
-	 * format
-	 *
-	 * Stores the archive format internally
-	 *
-	 * @access protected
-	 */
-	protected $format;
+    /**
+     * Stores the archive format internally
+     */
+    protected $format;
 
-	/**
-	 * fileName
-	 *
-	 * Stores the archive filename internally
-	 *
-	 * @access protected
-	 */
-	protected $fileName = '';
+    /**
+     * Stores the archive filename internally
+     */
+    protected $fileName = '';
 
-	/**
-	 * path
-	 *
-	 * Stores the archive path internally
-	 *
-	 * @access protected
-	 */
-	protected $path = '';
+    /**
+     * Stores the archive path internally
+     */
+    protected $path = '';
 
-	/**
-	 * prefix
-	 *
-	 * Stores the archive prefix internally
-	 *
-	 * @access protected
-	 */
-	protected $prefix = '';
+    /**
+     * Stores the archive prefix internally
+     */
+    protected $prefix = '';
 
-	/**
-	 * __construct
-	 *
-	 * Instantiates object
-	 *
-	 * @access public
-	 * @param mixed $gitObject the object
-	 * @param integer $format the format for the archive
-	 * @return mixed git archive
-	 */
-	public function __construct($project, $gitObject, $format = GITPHP_FORMAT_ZIP, $path = '', $prefix = '')
-	{
-		$this->SetProject($project);
-		$this->SetObject($gitObject);
-		$this->SetFormat($format);
-		$this->SetPath($path);
-		$this->SetPrefix($prefix);
-	}
+    /**
+     * __construct
+     *
+     * Instantiates object
+     *
+     * @param $project
+     * @param mixed $gitObject the object
+     * @param string $format the format for the archive
+     * @param string $path
+     * @param string $prefix
+     * @throws Exception
+     */
+    public function __construct($project, $gitObject, $format = self::GITPHP_COMPRESS_ZIP, $path = '', $prefix = '')
+    {
+        $this->SetProject($project);
+        $this->SetObject($gitObject);
+        $this->SetFormat($format);
+        $this->SetPath($path);
+        $this->SetPrefix($prefix);
+    }
 
-	/**
-	 * GetFormat
-	 *
-	 * Gets the archive format
-	 *
-	 * @access public
-	 * @return integer archive format
-	 */
-	public function GetFormat()
-	{
-		return $this->format;
-	}
+    /**
+     * GetFormat
+     *
+     * Gets the archive format
+     *
+     * @access public
+     * @return integer archive format
+     */
+    public function GetFormat()
+    {
+        return $this->format;
+    }
 
-	/**
-	 * SetFormat
-	 *
-	 * Sets the archive format
-	 *
-	 * @access public
-	 * @param integer $format archive format
-	 */
-	public function SetFormat($format)
-	{
-		if ((($format == self::GITPHP_COMPRESS_BZ2) && (!function_exists('bzcompress'))) ||
-		    (($format == self::GITPHP_COMPRESS_GZ) && (!function_exists('gzencode')))) {
-		    /*
-		     * Trying to set a format but doesn't have the appropriate
-		     * compression function, fall back to tar
-		     */
-		    $format = self::GITPHP_COMPRESS_TAR;
-		}
+    /**
+     * SetFormat
+     *
+     * Sets the archive format
+     *
+     * @param integer $format archive format
+     */
+    public function SetFormat($format)
+    {
+        if ((($format == self::GITPHP_COMPRESS_BZ2) && (!function_exists('bzcompress')))
+            || (($format == self::GITPHP_COMPRESS_GZ) && (!function_exists('gzencode')))) {
+            /*
+             * Trying to set a format but doesn't have the appropriate
+             * compression function, fall back to tar
+             */
+            $format = self::GITPHP_COMPRESS_TAR;
+        }
 
-		$this->format = $format;
-	}
+        $this->format = $format;
+    }
 
-	/**
-	 * GetObject
-	 *
-	 * Gets the object for this archive
-	 *
-	 * @access public
-	 * @return mixed the git object
-	 */
-	public function GetObject()
-	{
-		return $this->gitObject;
-	}
+    /**
+     * GetObject
+     *
+     * Gets the object for this archive
+     *
+     * @return mixed the git object
+     */
+    public function GetObject()
+    {
+        return $this->gitObject;
+    }
 
-	/**
-	 * SetObject
-	 *
-	 * Sets the object for this archive
-	 *
-	 * @access public
-	 * @param mixed $object the git object
-	 */
-	public function SetObject($object)
-	{
-		// Archive only works for commits and trees
-		if (($object != null) && (!(($object instanceof GitPHP_Commit) || ($object instanceof GitPHP_Tree)))) {
-			throw new Exception('Invalid source object for archive');
-		}
+    /**
+     * SetObject
+     *
+     * Sets the object for this archive
+     *
+     * @param mixed $object the git object
+     * @throws Exception
+     */
+    public function SetObject($object)
+    {
+        // Archive only works for commits and trees
+        if (($object != null) && (!(($object instanceof GitPHP_Commit) || ($object instanceof GitPHP_Tree)))) {
+            throw new Exception('Invalid source object for archive');
+        }
 
-		$this->gitObject = $object;
-	}
+        $this->gitObject = $object;
+    }
 
-	/**
-	 * GetProject
-	 *
-	 * Gets the project for this archive
-	 *
-	 * @access public
-	 * @return mixed the project
-	 */
-	public function GetProject()
-	{
-		if ($this->project)
-			return $this->project;
+    /**
+     * GetProject
+     *
+     * Gets the project for this archive
+     *
+     * @return mixed the project
+     */
+    public function GetProject()
+    {
+        if ($this->project) return $this->project;
 
-		if ($this->gitObject)
-			return $this->gitObject->GetProject();
+        if ($this->gitObject) return $this->gitObject->GetProject();
 
-		return null;
-	}
+        return null;
+    }
 
-	/**
-	 * SetProject
-	 *
-	 * Sets the project for this archive
-	 *
-	 * @access public
-	 * @param mixed $project the project
-	 */
-	public function SetProject($project)
-	{
-		$this->project = $project;
-	}
+    /**
+     * SetProject
+     *
+     * Sets the project for this archive
+     *
+     * @param mixed $project the project
+     */
+    public function SetProject($project)
+    {
+        $this->project = $project;
+    }
 
-	/**
-	 * GetExtension
-	 *
-	 * Gets the extension to use for this archive
-	 *
-	 * @access public
-	 * @return string extension for the archive
-	 */
-	public function GetExtension()
-	{
-		return GitPHP_Archive::FormatToExtension($this->format);
-	}
+    /**
+     * GetExtension
+     *
+     * Gets the extension to use for this archive
+     *
+     * @return string extension for the archive
+     * @throws Exception
+     */
+    public function GetExtension()
+    {
+        return GitPHP_Archive::FormatToExtension($this->format);
+    }
 
-	/**
-	 * GetFilename
-	 *
-	 * Gets the filename for this archive
-	 *
-	 * @access public
-	 * @return string filename
-	 */
-	public function GetFilename()
-	{
-		if (!empty($this->fileName)) {
-			return $this->fileName;
-		}
+    /**
+     * GetFilename
+     *
+     * Gets the filename for this archive
+     *
+     * @return string filename
+     * @throws Exception
+     */
+    public function GetFilename()
+    {
+        if (!empty($this->fileName)) {
+            return $this->fileName;
+        }
 
-		$fname = $this->GetProject()->GetSlug();
+        $fname = $this->GetProject()->GetSlug();
 
-		$fname .= '.' . $this->GetExtension();
+        $fname .= '.' . $this->GetExtension();
 
-		return $fname;
-	}
+        return $fname;
+    }
 
-	/**
-	 * SetFilename
-	 *
-	 * Sets the filename for this archive
-	 *
-	 * @access public
-	 * @param string $name filename
-	 */
-	public function SetFilename($name = '')
-	{
-		$this->fileName = $name;
-	}
+    /**
+     * SetFilename
+     *
+     * Sets the filename for this archive
+     *
+     * @param string $name filename
+     */
+    public function SetFilename($name = '')
+    {
+        $this->fileName = $name;
+    }
 
-	/**
-	 * GetPath
-	 *
-	 * Gets the path to restrict this archive to
-	 *
-	 * @access public
-	 * @return string path
-	 */
-	public function GetPath()
-	{
-		return $this->path;
-	}
+    /**
+     * GetPath
+     *
+     * Gets the path to restrict this archive to
+     *
+     * @return string path
+     */
+    public function GetPath()
+    {
+        return $this->path;
+    }
 
-	/**
-	 * SetPath
-	 *
-	 * Sets the path to restrict this archive to
-	 *
-	 * @access public
-	 * @param string $path path to restrict
-	 */
-	public function SetPath($path = '')
-	{
-		$this->path = $path;
-	}
+    /**
+     * SetPath
+     *
+     * Sets the path to restrict this archive to
+     *
+     * @param string $path path to restrict
+     */
+    public function SetPath($path = '')
+    {
+        $this->path = $path;
+    }
 
-	/**
-	 * GetPrefix
-	 *
-	 * Gets the directory prefix to use for files in this archive
-	 *
-	 * @access public
-	 * @return string prefix
-	 */
-	public function GetPrefix()
-	{
-		if (!empty($this->prefix)) {
-			return $this->prefix;
-		}
+    /**
+     * GetPrefix
+     *
+     * Gets the directory prefix to use for files in this archive
+     *
+     * @return string prefix
+     */
+    public function GetPrefix()
+    {
+        if (!empty($this->prefix)) {
+            return $this->prefix;
+        }
 
-		return $this->GetProject()->GetSlug() . '/';
-	}
+        return $this->GetProject()->GetSlug() . '/';
+    }
 
-	/**
-	 * SetPrefix
-	 *
-	 * Sets the directory prefix to use for files in this archive
-	 *
-	 * @access public
-	 * @param string $prefix prefix to use
-	 */
-	public function SetPrefix($prefix = '')
-	{
-		if (empty($prefix)) {
-			$this->prefix = $prefix;
-			return;
-		}
+    /**
+     * SetPrefix
+     *
+     * Sets the directory prefix to use for files in this archive
+     *
+     * @param string $prefix prefix to use
+     */
+    public function SetPrefix($prefix = '')
+    {
+        if (empty($prefix)) {
+            $this->prefix = $prefix;
+            return;
+        }
 
-		if (substr($prefix, -1) != '/') {
-			$prefix .= '/';
-		}
+        if (substr($prefix, -1) != '/') {
+            $prefix .= '/';
+        }
 
-		$this->prefix = $prefix;
-	}
+        $this->prefix = $prefix;
+    }
 
-	/**
-	 * GetHeaders
-	 *
-	 * Gets the headers to send to the browser for this file
-	 *
-	 * @access public
-	 * @return array header strings
-	 */
-	public function GetHeaders()
-	{
-		$headers = array();
+    /**
+     * GetHeaders
+     *
+     * Gets the headers to send to the browser for this file
+     *
+     * @return array header strings
+     * @throws Exception
+     */
+    public function GetHeaders()
+    {
+        $headers = array();
 
-		switch ($this->format) {
-			case self::GITPHP_COMPRESS_TAR:
-				$headers[] = 'Content-Type: application/x-tar';
-				break;
-			case self::GITPHP_COMPRESS_BZ2:
-				$headers[] = 'Content-Type: application/x-bzip2';
-				break;
-			case self::GITPHP_COMPRESS_GZ:
-				$headers[] = 'Content-Type: application/x-gzip';
-				break;
-			case self::GITPHP_COMPRESS_ZIP:
-				$headers[] = 'Content-Type: application/x-zip';
-				break;
-			default:
-				throw new Exception('Unknown compression type');
-		}
+        switch ($this->format) {
+            case self::GITPHP_COMPRESS_TAR:
+                $headers[] = 'Content-Type: application/x-tar';
+                break;
 
-		if (count($headers) > 0) {
-			$headers[] = 'Content-Disposition: attachment; filename=' . $this->GetFilename();
-		}
+            case self::GITPHP_COMPRESS_BZ2:
+                $headers[] = 'Content-Type: application/x-bzip2';
+                break;
 
-		return $headers;
-	}
+            case self::GITPHP_COMPRESS_GZ:
+                $headers[] = 'Content-Type: application/x-gzip';
+                break;
 
-	/**
-	 * GetData
-	 *
-	 * Gets the archive data
-	 *
-	 * @access public
-	 * @return string archive data
-	 */
-	public function GetData()
-	{
-		if (!$this->gitObject)
-		{
-			throw new Exception('Invalid object for archive');
-		}
+            case self::GITPHP_COMPRESS_ZIP:
+                $headers[] = 'Content-Type: application/x-zip';
+                break;
 
-		$exe = new GitPHP_GitExe($this->GetProject());
+            default:
+                throw new Exception('Unknown compression type');
+        }
 
-		$args = array();
+        if (count($headers) > 0) {
+            $headers[] = 'Content-Disposition: attachment; filename=' . $this->GetFilename();
+        }
 
-		switch ($this->format) {
-			case self::GITPHP_COMPRESS_ZIP:
-				$args[] = '--format=zip';
-				break;
-			case self::GITPHP_COMPRESS_TAR:
-			case self::GITPHP_COMPRESS_BZ2:
-			case self::GITPHP_COMPRESS_GZ:
-				$args[] = '--format=tar';
-				break;
-		}
+        return $headers;
+    }
 
-		$args[] = '--prefix=' . $this->GetPrefix();
-		$args[] = $this->gitObject->GetHash();
+    /**
+     * GetData
+     *
+     * Gets the archive data
+     *
+     * @return string archive data
+     * @throws Exception
+     */
+    public function GetData()
+    {
+        if (!$this->gitObject) {
+            throw new Exception('Invalid object for archive');
+        }
 
-		if (!empty($this->path))
-			$args[] = $this->path;
+        $exe = new GitPHP_GitExe($this->GetProject());
 
-		$data = $exe->Execute(GIT_ARCHIVE, $args);
-		unset($exe);
+        $args = array();
 
-		switch ($this->format) {
-			case self::GITPHP_COMPRESS_BZ2:
-				$data = bzcompress($data, GitPHP_Config::GetInstance()->GetValue('compresslevel', 4));
-				break;
-			case self::GITPHP_COMPRESS_GZ:
-				$data = gzencode($data, GitPHP_Config::GetInstance()->GetValue('compresslevel', -1));
-				break;
-		}
+        switch ($this->format) {
+            case self::GITPHP_COMPRESS_ZIP:
+                $args[] = '--format=zip';
+                break;
 
-		return $data;
-	}
+            case self::GITPHP_COMPRESS_TAR:
+            case self::GITPHP_COMPRESS_BZ2:
+            case self::GITPHP_COMPRESS_GZ:
+                $args[] = '--format=tar';
+                break;
+        }
 
-	/**
-	 * FormatToExtension
-	 *
-	 * Gets the extension to use for a particular format
-	 *
-	 * @access public
-	 * @static
-	 * @param string $format format to get extension for
-	 * @return string file extension
-	 */
-	public static function FormatToExtension($format)
-	{
-		switch ($format) {
-			case self::GITPHP_COMPRESS_TAR:
-				return 'tar';
-				break;
-			case self::GITPHP_COMPRESS_BZ2:
-				return 'tar.bz2';
-				break;
-			case self::GITPHP_COMPRESS_GZ:
-				return 'tar.gz';
-				break;
-			case self::GITPHP_COMPRESS_ZIP:
-				return 'zip';
-				break;
-		}
-	}
+        $args[] = '--prefix=' . $this->GetPrefix();
+        $args[] = $this->gitObject->GetHash();
 
-	/**
-	 * SupportedFormats
-	 *
-	 * Gets the supported formats for the archiver
-	 *
-	 * @access public
-	 * @static
-	 * @return array array of formats mapped to extensions
-	 */
-	public static function SupportedFormats()
-	{
-		$formats = array();
+        if (!empty($this->path)) $args[] = $this->path;
 
-		$formats[self::GITPHP_COMPRESS_TAR] = GitPHP_Archive::FormatToExtension(self::GITPHP_COMPRESS_TAR);
-		
-		// TODO check for git > 1.4.3 for zip
-		$formats[self::GITPHP_COMPRESS_ZIP] = GitPHP_Archive::FormatToExtension(self::GITPHP_COMPRESS_ZIP);
+        $data = $exe->Execute(GIT_ARCHIVE, $args);
+        unset($exe);
 
-		if (function_exists('bzcompress'))
-			$formats[self::GITPHP_COMPRESS_BZ2] = GitPHP_Archive::FormatToExtension(self::GITPHP_COMPRESS_BZ2);
+        switch ($this->format) {
+            case self::GITPHP_COMPRESS_BZ2:
+                $data = bzcompress($data, GitPHP_Config::GetInstance()->GetValue('compresslevel', 4));
+                break;
 
-		if (function_exists('gzencode'))
-			$formats[self::GITPHP_COMPRESS_GZ] = GitPHP_Archive::FormatToExtension(self::GITPHP_COMPRESS_GZ);
+            case self::GITPHP_COMPRESS_GZ:
+                $data = gzencode($data, GitPHP_Config::GetInstance()->GetValue('compresslevel', -1));
+                break;
+        }
 
-		return $formats;
-	}
+        return $data;
+    }
 
+    /**
+     * FormatToExtension
+     *
+     * Gets the extension to use for a particular format
+     *
+     * @param string $format format to get extension for
+     * @return string file extension
+     * @throws Exception
+     */
+    public static function FormatToExtension($format)
+    {
+        switch ($format) {
+            case self::GITPHP_COMPRESS_TAR:
+                return 'tar';
+                break;
+
+            case self::GITPHP_COMPRESS_BZ2:
+                return 'tar.bz2';
+                break;
+
+            case self::GITPHP_COMPRESS_GZ:
+                return 'tar.gz';
+                break;
+
+            case self::GITPHP_COMPRESS_ZIP:
+                return 'zip';
+                break;
+
+            default:
+                throw new \Exception('Unknown compression type');
+        }
+    }
+
+    /**
+     * SupportedFormats
+     *
+     * Gets the supported formats for the archiver
+     *
+     * @access public
+     * @static
+     * @return array array of formats mapped to extensions
+     * @throws Exception
+     */
+    public static function SupportedFormats()
+    {
+        $formats = array();
+        $formats[self::GITPHP_COMPRESS_TAR] = GitPHP_Archive::FormatToExtension(self::GITPHP_COMPRESS_TAR);
+        $formats[self::GITPHP_COMPRESS_ZIP] = GitPHP_Archive::FormatToExtension(self::GITPHP_COMPRESS_ZIP);
+
+        if (function_exists('bzcompress')) {
+            $formats[self::GITPHP_COMPRESS_BZ2] = GitPHP_Archive::FormatToExtension(self::GITPHP_COMPRESS_BZ2);
+        }
+        if (function_exists('gzencode')) {
+            $formats[self::GITPHP_COMPRESS_GZ] = GitPHP_Archive::FormatToExtension(self::GITPHP_COMPRESS_GZ);
+        }
+
+        return $formats;
+    }
 }
