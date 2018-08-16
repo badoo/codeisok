eed5bc23ccf9949c39ecc8b4f43ebdd453f1b030
Aleksandr Izmailov <ad.izmailov@gmail.com>
2018-08-16 22:13:27 +0300

    formatting fixes; added ToArray method to prevent problems with array_map method

--- .include/git/TreeDiff.class.php
+++ .include/git/TreeDiff.class.php
@@ -18,60 +18,53 @@
  */
 class GitPHP_TreeDiff implements Iterator
 {
-	
-	/**
+    /**
 	 * fromHash
 	 *
 	 * Stores the from hash
-	 *
-	 * @access protected
 	 */
-	protected $fromHash;
+	    protected $fromHash;
 
-	/**
+    /**
 	 * toHash
 	 *
 	 * Stores the to hash
-	 *
-	 * @access protected
 	 */
-	protected $toHash;
+	    protected $toHash;
 
-	/**
+    /**
 	 * renames
 	 *
 	 * Stores whether to detect renames
-	 *
-	 * @access protected
 	 */
-	protected $renames;
+	    protected $renames;
 
-	/**
+    /**
 	 * project
 	 *
 	 * Stores the project
-	 *
-	 * @access protected
 	 */
-	protected $project;
+	    protected $project;
 
-	/**
+    /**
 	 * fileDiffs
 	 *
 	 * Stores the individual file diffs
-	 *
-	 * @access protected
+     * @var GitPHP_FileDiff[]
 	 */
-	protected $fileDiffs = array();
+	    protected $fileDiffs = array();
 
-	/**
+    /**
 	 * dataRead
 	 *
 	 * Stores whether data has been read
-	 *
-	 * @access protected
 	 */
-	protected $dataRead = false;
+	    protected $dataRead = false;
+
+    /**
+     * @var DiffContext
+     */
+	    protected $DiffContext;
 
     /**
      * @param GitPHP_Project $project project
@@ -80,70 +73,66 @@ class GitPHP_TreeDiff implements Iterator
      * @param DiffContext $DiffContext
      * @throws GitPHP_MessageException
      */
-	public function __construct($project, $toHash, $fromHash = '', DiffContext $DiffContext)
-	{
-		$this->project = $project;
+	    public function __construct($project, $toHash, $fromHash = '', DiffContext $DiffContext)
+    {
+        $this->project = $project;
         $this->DiffContext = $DiffContext;
 
-		$toCommit = $this->project->GetCommit($toHash);
+        $toCommit = $this->project->GetCommit($toHash);
         if (empty($toCommit)) {
             throw new GitPHP_MessageException('Commit not found ' . $toHash);
         }
-		$this->toHash = $toHash;
+        $this->toHash = $toHash;
 
-		if (empty($fromHash)) {
-			$parent = $toCommit->GetParent();
-			if ($parent) {
-				$this->fromHash = $parent->GetHash();
-			}
-		} else {
-			$this->fromHash = $fromHash;
-		}
-	}
+        if (empty($fromHash)) {
+            $parent = $toCommit->GetParent();
+            if ($parent) {
+                $this->fromHash = $parent->GetHash();
+            }
+        } else {
+            $this->fromHash = $fromHash;
+        }
+    }
 
-	/**
+    /**
 	 * ReadData
 	 *
 	 * Reads the tree diff data
 	 *
 	 * @access private
 	 */
-	private function ReadData()
-	{
-		$this->dataRead = true;
+	    private function ReadData()
+    {
+        $this->dataRead = true;
 
-		$this->fileDiffs = array();
+        $this->fileDiffs = array();
 
-		$exe = new GitPHP_GitExe($this->project);
+        $exe = new GitPHP_GitExe($this->project);
 
-		$args = array();
+        $args = array();
 
-		$args[] = '-r';
-		if ($this->DiffContext->getRenames())
-			$args[] = '-M';
+        $args[] = '-r';
+        if ($this->DiffContext->getRenames()) $args[] = '-M';
 
-		if (empty($this->fromHash))
-			$args[] = '--root';
-		else
-			$args[] = $this->fromHash;
+        if (empty($this->fromHash)) $args[] = '--root';
+        else $args[] = $this->fromHash;
 
-		$args[] = $this->toHash;
+        $args[] = $this->toHash;
 
-		$diffTreeLines = explode("\n", $exe->Execute(GIT_DIFF_TREE, $args));
-		foreach ($diffTreeLines as $line) {
-			$trimmed = trim($line);
-			if ((strlen($trimmed) > 0) && (substr_compare($trimmed, ':', 0, 1) === 0)) {
-				try {
+        $diffTreeLines = explode("\n", $exe->Execute(GIT_DIFF_TREE, $args));
+        foreach ($diffTreeLines as $line) {
+            $trimmed = trim($line);
+            if ((strlen($trimmed) > 0) && (substr_compare($trimmed, ':', 0, 1) === 0)) {
+                try {
                     $this->fileDiffs[] = new GitPHP_FileDiff($this->project, $trimmed, $this->toHash, $this->DiffContext);
-				} catch (Exception $e) {
-				}
-			}
-		}
+                } catch (Exception $e) {}
+            }
+        }
 
-		unset($exe);
-	}
+        unset($exe);
+    }
 
-	/**
+    /**
 	 * GetFromHash
 	 *
 	 * Gets the from hash for this treediff
@@ -151,12 +140,12 @@ class GitPHP_TreeDiff implements Iterator
 	 * @access public
 	 * @return string from hash
 	 */
-	public function GetFromHash()
-	{
-		return $this->fromHash;
-	}
+	    public function GetFromHash()
+    {
+        return $this->fromHash;
+    }
 
-	/**
+    /**
 	 * GetToHash
 	 *
 	 * Gets the to hash for this treediff
@@ -164,12 +153,12 @@ class GitPHP_TreeDiff implements Iterator
 	 * @access public
 	 * @return string to hash
 	 */
-	public function GetToHash()
-	{
-		return $this->toHash;
-	}
+	    public function GetToHash()
+    {
+        return $this->toHash;
+    }
 
-	/**
+    /**
 	 * GetRenames
 	 *
 	 * Get whether this treediff is set to detect renames
@@ -177,12 +166,12 @@ class GitPHP_TreeDiff implements Iterator
 	 * @access public
 	 * @return boolean true if renames will be detected
 	 */
-	public function GetRenames()
-	{
-		return $this->renames;
-	}
+	    public function GetRenames()
+    {
+        return $this->renames;
+    }
 
-	/**
+    /**
 	 * SetRenames
 	 *
 	 * Set whether this treediff is set to detect renames
@@ -190,81 +179,75 @@ class GitPHP_TreeDiff implements Iterator
 	 * @access public
 	 * @param boolean $renames whether to detect renames
 	 */
-	public function SetRenames($renames)
-	{
-		if ($renames == $this->renames)
-			return;
+	    public function SetRenames($renames)
+    {
+        if ($renames == $this->renames) return;
 
-		$this->renames = $renames;
-		$this->dataRead = false;
-	}
+        $this->renames = $renames;
+        $this->dataRead = false;
+    }
 
-	/**
+    /**
 	 * rewind
 	 *
 	 * Rewinds the iterator
 	 */
-	function rewind()
-	{
-		if (!$this->dataRead)
-			$this->ReadData();
+	    function rewind()
+    {
+        if (!$this->dataRead) $this->ReadData();
 
-		return reset($this->fileDiffs);
-	}
+        return reset($this->fileDiffs);
+    }
 
-	/**
+    /**
 	 * current
 	 *
 	 * Returns the current element in the array
 	 */
-	function current()
-	{
-		if (!$this->dataRead)
-			$this->ReadData();
+	    function current()
+    {
+        if (!$this->dataRead) $this->ReadData();
 
-		return current($this->fileDiffs);
-	}
+        return current($this->fileDiffs);
+    }
 
-	/**
+    /**
 	 * key
 	 *
 	 * Returns the current key
 	 */
-	function key()
-	{
-		if (!$this->dataRead)
-			$this->ReadData();
+	    function key()
+    {
+        if (!$this->dataRead) $this->ReadData();
 
-		return key($this->fileDiffs);
-	}
+        return key($this->fileDiffs);
+    }
 
-	/**
+    /**
 	 * next
 	 *
 	 * Advance the pointer
 	 */
-	function next()
-	{
-		if (!$this->dataRead)
-			$this->ReadData();
+	    function next()
+    {
+        if (!$this->dataRead) $this->ReadData();
 
-		return next($this->fileDiffs);
-	}
+        return next($this->fileDiffs);
+    }
 
-	/**
+    /**
 	 * valid
 	 *
 	 * Test for a valid pointer
 	 */
-	function valid()
-	{
-		if (!$this->dataRead)
-			$this->ReadData();
+	    function valid()
+    {
+        if (!$this->dataRead) $this->ReadData();
 
-		return key($this->fileDiffs) !== null;
-	}
+        return key($this->fileDiffs) !== null;
+    }
 
-	/**
+    /**
 	 * Count
 	 *
 	 * Gets the number of file changes in this treediff
@@ -272,12 +255,17 @@ class GitPHP_TreeDiff implements Iterator
 	 * @access public
 	 * @return integer count of file changes
 	 */
-	public function Count()
-	{
-		if (!$this->dataRead)
-			$this->ReadData();
+	    public function Count()
+    {
+        if (!$this->dataRead) $this->ReadData();
 
-		return count($this->fileDiffs);
-	}
+        return count($this->fileDiffs);
+    }
 
+    public function ToArray()
+    {
+        if (!$this->dataRead) $this->ReadData();
+
+        return $this->fileDiffs;
+    }
 }
