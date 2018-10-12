6c9554666cb4d591f9459aee8bd55d2f7b64ba81
Aleksandr Izmailov <ad.izmailov@gmail.com>
2018-10-12 20:12:52 +0300

    fix warning when calling mysqli_num_rows() with bool argument
    mysqli_query might return TRUE in some cases

--- .include/Db.php
+++ .include/Db.php
@@ -166,7 +166,7 @@ class Db
             trigger_error($msg);
             \GitPHP\Log::GetInstance()->Log('mysqli_error', "$this->errno: $this->error");
         } else {
-            $this->numRows = mysqli_num_rows($result);
+            if ($result instanceof \mysqli_result) $this->numRows = mysqli_num_rows($result);
             $this->affectedRows = mysqli_affected_rows($this->link);
             $this->insert_id = mysqli_insert_id($this->link);
             $result = new Db_Result($result);
