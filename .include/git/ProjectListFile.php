0587d41b60a90e781c71ec48125f9994b8fc68b2
Maria Akinfieva <m.akinfieva@corp.badoo.com>
2020-07-31 18:23:10 +0100

    move ProjectListFile to GitPHP/Git namespace

--- .include/git/ProjectListFile.php
+++ .include/git/ProjectListFile.php
@@ -1,22 +1,8 @@
 <?php
-/**
- * GitPHP ProjectListFile
- *
- * Lists all projects in a given file
- *
- * @author Christopher Han <xiphux@gmail.com>
- * @copyright Copyright (c) 2010 Christopher Han
- * @package GitPHP
- * @subpackage Git
- */
 
-/**
- * ProjectListFile class
- *
- * @package GitPHP
- * @subpackage Git
- */
-class GitPHP_ProjectListFile extends \GitPHP\Git\ProjectListBase
+namespace GitPHP\Git;
+
+class ProjectListFile extends \GitPHP\Git\ProjectListBase
 {
     /**
      * __construct
@@ -24,13 +10,13 @@ class GitPHP_ProjectListFile extends \GitPHP\Git\ProjectListBase
      * constructor
      *
      * @param string $projectFile file to read
-     * @throws Exception if parameter is not a readable file
+     * @throws \Exception if parameter is not a readable file
      * @access public
      */
     public function __construct($projectFile)
     {
         if (!(is_string($projectFile) && is_file($projectFile))) {
-            throw new Exception(sprintf(__('%1$s is not a file'), $projectFile));
+            throw new \Exception(sprintf(__('%1$s is not a file'), $projectFile));
         }
 
         $this->projectConfig = $projectFile;
@@ -44,12 +30,12 @@ class GitPHP_ProjectListFile extends \GitPHP\Git\ProjectListBase
      * Populates the internal list of projects
      *
      * @access protected
-     * @throws Exception if file cannot be read
+     * @throws \Exception if file cannot be read
      */
     protected function PopulateProjects()
     {
         if (!($fp = fopen($this->projectConfig, 'r'))) {
-            throw new Exception(sprintf(__('Failed to open project list file %1$s'), $this->projectConfig));
+            throw new \Exception(sprintf(__('Failed to open project list file %1$s'), $this->projectConfig));
         }
 
         $projectRoot = \GitPHP\Util::AddSlash(\GitPHP\Config::GetInstance()->GetValue(\GitPHP\Config::PROJECT_ROOT));
@@ -66,7 +52,7 @@ class GitPHP_ProjectListFile extends \GitPHP\Git\ProjectListBase
                             }
                         }
                         $this->projects[$regs[1]] = $projObj;
-                    } catch (Exception $e) {}
+                    } catch (\Exception $e) {}
                 }
             }
         }
