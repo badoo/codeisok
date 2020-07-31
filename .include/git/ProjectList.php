567586f69ad7e7afb311a4978909e98fe18fcb7d
Maria Akinfieva <m.akinfieva@corp.badoo.com>
2020-07-31 18:20:05 +0100

    move ProjectListDirectory to GitPHP/Git namespace

--- .include/git/ProjectList.php
+++ .include/git/ProjectList.php
@@ -58,7 +58,7 @@ class ProjectList
             }
         }
 
-        if (!self::$instance) self::$instance = new \GitPHP_ProjectListDirectory(\GitPHP\Config::GetInstance()->GetValue(\GitPHP\Config::PROJECT_ROOT));
+        if (!self::$instance) self::$instance = new \GitPHP\Git\ProjectListDirectory(\GitPHP\Config::GetInstance()->GetValue(\GitPHP\Config::PROJECT_ROOT));
 
         if (isset($git_projects_settings) && !$legacy) self::$instance->ApplySettings($git_projects_settings);
     }
